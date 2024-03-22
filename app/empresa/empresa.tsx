e8b23d46aed6a0c4bb240aca489aa93b923b1882
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-03-22 12:28:29 -0600

    first commit

---
+++ app/empresa/empresa.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Empresa = () => {
+  return (
+    <div>
+      <p>Hola</p>
+    </div>
+  );
+}
+
+export default Empresa;
---
+++ components/Navigation.tsx
@@ -0,0 +1,14 @@
+const Navigation = () =>{
+    return(
+        <div>
+            <nav className="absolute top-5 right-0 z-10">
+            <a href="/" className="text-black hover:text-green-900 px-4 py-2 border-r border-black">Home</a>
+            <a href="./Empresa" className="text-black hover:text-green-900 px-4 py-2 border-r border-black">Empresa</a>
+            <a href="/proyecto" className="text-black hover:text-green-900 px-4 py-2 border-r border-black">Proyecto</a>
+            <a href="/nosotros" className="text-black hover:text-green-900 px-4 py-2 border-r border-black">Nosotros</a>
+            </nav>
+        </div>
+    );
+ }
+
+ export default Navigation;
